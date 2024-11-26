17de2bbd393b87120e3f4ebdebf9b7265c255719
Dagson Gabriel <dagson.g.01@gmail.com>
2024-11-25 23:18:53 -0300

    feat: configuração inicial das rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navbar ><h1>Home</h1></Navbar>,
+  },
+  {
+    path: '/products',
+    element: <Navbar ><h1>Produtos</h1></Navbar>,
+  },
+  {
+    path: '/tables',
+    element: <Navbar ><h1>Mesas</h1></Navbar>,
+  },
+  {
+    path: '/reports',
+    element: <Navbar ><h1>RelaTórios</h1></Navbar>,
+  },
+  {
+    path: '/stock',
+    element: <Navbar ><h1>Estoque</h1></Navbar>,
+  },
+  {
+    path: '/orders',
+    element: <Navbar ><h1>Pedidos</h1></Navbar>,
+  },
+  {
+    path: '/users',
+    element: <Navbar ><h1>Usuários</h1></Navbar>,
+  },
+  {
+    path: '/settings',
+    element: <Navbar ><h1>Configurações</h1></Navbar>,
+  },
+  {
+    path: '*',
+    element: <div>Not Found</div>,
+  }
+]);
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
