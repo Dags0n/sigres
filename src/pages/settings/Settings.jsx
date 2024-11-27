aa92b44e61b32a9828b0061054ee9383908340a3
gili-julio <giliardoplay@gmail.com>
2024-11-27 06:24:38 -0300

    feat: adiciona foto de perfil na página de configurações

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Settings.css";
 import { Typography } from "@mui/material";
+import CaraTranquilo from '../../assets/cara-tranquilo.png';
 
 const Settings = () => {
     const [showNotification, setShowNotification] = useState(false);
@@ -26,33 +27,45 @@ const Settings = () => {
                 Configurações
             </Typography>
             <div className="configuracoes-box">
-                <div className="configuracoes-row">
-                    <label>Nome do Estabelecimento</label>
-                    <input type="text" defaultValue="Exemplo's Bar" className="configuracoes-input" />
-                </div>
-                <div className="configuracoes-row">
-                    <label>Fuso Horário</label>
-                    <select defaultValue="Brasília, DF (GMT-3)" className="configuracoes-input">
-                        <option>Brasília, DF (GMT-3)</option>
-                        <option>São Paulo, SP (GMT-3)</option>
-                        <option>Nova York, NY (GMT-5)</option>
-                    </select>
-                </div>
-                <div className="configuracoes-row">
-                    <label>Usar senha para entrar no SIGRes</label>
-                    <input type="checkbox" className="configuracoes-checkbox" />
-                </div>
-                <div className="configuracoes-row">
-                    <label>Senha de acesso ao SIGRes</label>
-                    <input type="password" defaultValue="******" className="configuracoes-input" />
-                </div>
-                <div className="configuracoes-row">
-                    <label>Bate-papo ativo</label>
-                    <input type="checkbox" className="configuracoes-checkbox" />
-                </div>
-                <div className="configuracoes-row">
-                    <label>Número de Mesas</label>
-                    <input type="number" defaultValue="200" className="configuracoes-input" />
+                <div className="flexx">
+                    <div className="configuracoes-row">
+                        <label>Nome do Estabelecimento</label>
+                        <input type="text" defaultValue="Exemplo's Bar" className="configuracoes-input" />
+                    </div>
+                    <div className="configuracoes-row">
+                        <label>Fuso Horário</label>
+                        <select defaultValue="Brasília, DF (GMT-3)" className="configuracoes-input">
+                            <option>Brasília, DF (GMT-3)</option>
+                            <option>São Paulo, SP (GMT-3)</option>
+                            <option>Nova York, NY (GMT-5)</option>
+                        </select>
+                    </div>
+                    <div className="configuracoes-row">
+                        <label>Senha de acesso ao SIGRes</label>
+                        <input type="password" defaultValue="******" className="configuracoes-input" />
+                    </div>
+                    <div className="configuracoes-row">
+                        <label>Número de Mesas</label>
+                        <input type="number" defaultValue="200" className="configuracoes-input" />
+                    </div>
+                    <div className="form-group avatar-section">
+                        <img
+                            src={CaraTranquilo}
+                            alt="Avatar"
+                            className="avatar-icon"
+                        />
+                        <button type="button" className="alterar-icone-button">
+                            Alterar Ícone
+                        </button>
+                    </div>
+                    <div className="configuracoes-row">
+                        <label>Usar senha para entrar no SIGRes</label>
+                        <input type="checkbox" className="configuracoes-checkbox" />
+                    </div>
+                    <div className="configuracoes-row">
+                        <label>Bate-papo ativo</label>
+                        <input type="checkbox" className="configuracoes-checkbox" />
+                    </div>
                 </div>
                 <button id="configuracoes-button" className="configuracoes-button" onClick={handleConfirm}>Salvar Alterações</button>
             </div>
