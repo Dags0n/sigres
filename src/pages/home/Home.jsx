e3332b885d351b256bdf4aca080125e3a5beaca8
Dagson Gabriel <dagson.g.01@gmail.com>
2025-01-20 06:12:46 -0300

    feat: adiciona dados iniciais na página home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,21 +3,163 @@ import { Typography } from '@mui/material';
 import ItemCounterBox from '../../components/item-counter-box/ItemCounterBox';
 import LineChart from '../../components/line-chart/LineChart';
 
-// Dados para o ItemCounterBox
-const items = [
-  { name: 'Produtos Cadastrados', count: 12 },
-  { name: 'Mesas Cadastradas', count: 25 },
-  { name: 'Usuários Cadastrados', count: 25 },
-  { name: 'Produtos no Estoque', count: 8 },
-  { name: 'Pedidos Realizados', count: 1500 },
-];
-
 // Dados para o gráfico
 const xAxis = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'];
 const yAxis = 'Vendas Realizadas na semana';
 const data = [100, 20, 150, 25, 300, 35, 20];
 
 export default function Home() {
+  const [items, setItems] = React.useState({
+    products: { name: 'Produtos Cadastrados', count: 0 },
+    tables: { name: 'Mesas Cadastradas', count: 0 },
+    users: { name: 'Usuários Cadastrados', count: 0 },
+    stock: { name: 'Produtos no Estoque', count: 0 },
+    orders: { name: 'Pedidos Realizados', count: 0 },
+  });
+
+  const updateItemsCount = (itemId, count) => {
+    setItems((prevItems) => {
+      return {
+        ...prevItems,
+        [itemId]: {
+          ...prevItems[itemId],
+          count,
+        },
+      };
+    });
+  }
+
+  React.useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch('http://localhost:8080/product-variant', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setItems(
+            updateItemsCount('products', data.length),
+          );
+        } else {
+          console.error('Erro ao buscar itens');
+        }
+      } catch (error) {
+        console.error('Erro ao fazer a requisição', error);
+      }
+    }
+    fetchItems();
+  }, []);
+
+  React.useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch('http://localhost:8080/desk', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setItems(
+            updateItemsCount('tables', data.length),
+          );
+        } else {
+          console.error('Erro ao buscar itens');
+        }
+      } catch (error) {
+        console.error('Erro ao fazer a requisição', error);
+      }
+    }
+    fetchItems();
+  }, []);
+  
+  React.useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch('http://localhost:8080/user', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setItems(
+            updateItemsCount('users', data.length),
+          );
+        } else {
+          console.error('Erro ao buscar itens');
+        }
+      } catch (error) {
+        console.error('Erro ao fazer a requisição', error);
+      }
+    }
+    fetchItems();
+  }, []);
+
+  React.useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch('http://localhost:8080/order', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setItems(
+            updateItemsCount('orders', data.length),
+          );
+        } else {
+          console.error('Erro ao buscar itens');
+        }
+      } catch (error) {
+        console.error('Erro ao fazer a requisição', error);
+      }
+    }
+    fetchItems();
+  }, []);
+
+  React.useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch('http://localhost:8080/product-variant', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          products = data.filter((item) => item.amount > 0);
+          setItems(
+            updateItemsCount('stock', products.length),
+          );
+        } else {
+          console.error('Erro ao buscar itens');
+        }
+      } catch (error) {
+        console.error('Erro ao fazer a requisição', error);
+      }
+    }
+    fetchItems();
+  }, []);
+
   return (
     <>
       <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'start', flexWrap: 'wrap' }}>
@@ -25,7 +167,7 @@ export default function Home() {
           <Typography variant="h4" sx={{ fontFamily: 'Poppins', fontWeight: 900, m: "0 0 10px 0", justifySelf: 'flex-start' }}>
             Dashboard
           </Typography>
-          <ItemCounterBox items={items} />
+          <ItemCounterBox items={Object.values(items)} />
         </div>
         <div style={{ width: '100%', display: 'flex', alignItems: 'start', justifyContent: 'start', flexDirection: 'column' }}>
           <Typography variant="h4" sx={{ fontFamily: 'Poppins', fontWeight: 900, m: "35px 0 10px 0" }}>
